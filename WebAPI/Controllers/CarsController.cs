88ec4cc1905fc8819f9fb0946b1c1e558f5b185f
yagmurlu <mavibulut0106@gmail.com>
2022-08-26 20:22:46 +0300

    API added

---
+++ WebAPI/Controllers/CarsController.cs
@@ -0,0 +1,33 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        ICarService _carService;
+
+        public CarsController(ICarService carService)
+        {
+            _carService = carService;
+        }
+
+        [HttpGet]
+        public List<Car> Get()
+        {
+             
+            var result = _carService.GetAll();
+            return result.Data;
+        }
+    }
+}
